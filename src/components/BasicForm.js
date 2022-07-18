0173ae30bc39ff9a16c6e53e2ca45a6f82af4070
pedroDeAlba123 <pedrodealbaalegre@gmail.com>
2022-07-18 11:14:29 -0300

    added basic validation to email

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -19,12 +19,15 @@ const BasicForm = (props) => {
   const formIsValidHandler = (event) => {
     event.preventDefault();
 
-    if (nameIsValid && enteredInputNameIsInvalid) {
+    if (nameIsValid && lastNameValid && emailIsValid) {
       setEnteredName("");
       setEnteredNameTouch(false);
 
       setEnteredLastName("");
       setEnteredLastNameTouch(false);
+
+      setEnteredEmail("");
+      setEnteredEmailTouch(false);
     }
   };
 
@@ -43,10 +46,31 @@ const BasicForm = (props) => {
   const lastNameOnBlurHandler = () => {
     setEnteredLastNameTouch(true);
   };
-  const lastNameValid = enteredLastName.trim() !== '';
-  const enteredLastNameIsInvalid = enteredLastNameTouch && !lastNameValid; 
+  const lastNameValid = enteredLastName.trim() !== "";
+  const enteredLastNameIsInvalid = enteredLastNameTouch && !lastNameValid;
 
   const lastNameClassType = enteredLastNameIsInvalid
+    ? "form-control invalid"
+    : "form-control";
+
+  //E-Mail
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredEmailTouch, setEnteredEmailTouch] = useState(false);
+
+  const enteredEmailHandler = (event) => {
+    setEnteredEmail(event.target.value);
+  };
+
+  const onBlurEmailHandler = () => {
+    setEnteredEmailTouch(true);
+  };
+
+  const emailIsValid =
+    enteredEmail.trim() !== "" &&
+    /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(enteredEmail);
+  const emailIsInvalid = !emailIsValid && enteredEmailTouch;
+
+  const emailClassType = emailIsInvalid
     ? "form-control invalid"
     : "form-control";
 
@@ -75,12 +99,23 @@ const BasicForm = (props) => {
             value={enteredLastName}
             onBlur={lastNameOnBlurHandler}
           />
-          {enteredLastNameIsInvalid && <p className="error-text">Please set a valid Last Name!</p>}
+          {enteredLastNameIsInvalid && (
+            <p className="error-text">Please set a valid Last Name!</p>
+          )}
         </div>
       </div>
-      <div className="form-control">
+      <div className={emailClassType}>
         <label htmlFor="email">E-Mail Address</label>
-        <input type="text" id="email" />
+        <input
+          type="text"
+          id="email"
+          onChange={enteredEmailHandler}
+          value={enteredEmail}
+          onBlur={onBlurEmailHandler}
+        />
+        {emailIsInvalid && (
+          <p className={"error-text"}>Please set a valid email address!</p>
+        )}
       </div>
       <div className="form-actions">
         <button>Submit</button>
